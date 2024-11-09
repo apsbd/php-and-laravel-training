7dfcf2c6d8d9c5cba5ca57006ea0b53b9abd97b3
Mohiuddin Ahmed <mohiuddin@tieredcommunication.com>
2024-11-09 08:13:06 +0600

    Initial Commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Models\Blog;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,46 +9,12 @@ Route::get('/', function () {
 
 Route::get('/blogs', function () {
     return view('blogs', [
-        'blogs' => [
-            [
-                'id' => 1,
-                'title' => 'My first blog',
-                'body' => 'This is my first blog post'
-            ],
-            [
-                'id' => 2,
-                'title' => 'My second blog',
-                'body' => 'This is my second blog post'
-            ],
-            [
-                'id' => 3,
-                'title' => 'My third blog',
-                'body' => 'This is my third blog post'
-            ]
-        ]
+        'blogs' => Blog::all()
     ]);
 });
 
 Route::get('/blogs/{id}', function ($id) {
-    $blogs = [
-        [
-            'id' => 1,
-            'title' => 'My first blog',
-            'body' => 'This is my first blog post'
-        ],
-        [
-            'id' => 2,
-            'title' => 'My second blog',
-            'body' => 'This is my second blog post'
-        ],
-        [
-            'id' => 3,
-            'title' => 'My third blog',
-            'body' => 'This is my third blog post'
-        ]
-    ];
-
-    $blog = Arr::first($blogs, fn ($blog) => $blog['id'] == $id);
+    $blog = Blog::find($id);
 
     return view('blog', ['blog' => $blog]);
 });
