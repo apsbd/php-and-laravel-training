6fe7c1f0c8ba49d7aa095452f3f7cc491af2cec9
Mohiuddin Ahmed <mohiuddin@tieredcommunication.com>
2024-11-09 07:48:07 +0600

    Initial Commit

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -21,7 +21,6 @@
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
                             <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-                            <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
                             <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
                         </div>
                     </div>
@@ -71,7 +70,6 @@
             <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                 <a href="/" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
-                <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About</a>
                 <a href="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact</a>
             </div>
             <div class="border-t border-gray-700 pb-3 pt-4">
@@ -80,8 +78,8 @@
                         <img class="h-10 w-10 rounded-full" src="https://eu.ui-avatars.com/api/?name=Laravel+Training&size=250" alt="">
                     </div>
                     <div class="ml-3">
-                        <div class="text-base font-medium leading-none text-white">Lary Robot</div>
-                        <div class="text-sm font-medium leading-none text-gray-400">jeffrey@laracasts.com</div>
+                        <div class="text-base font-medium leading-none text-white">Mohiuddin Ahmed</div>
+                        <div class="text-sm font-medium leading-none text-gray-400">ahmed@tieredcommunication.com</div>
                     </div>
                     <button type="button" class="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                         <span class="absolute -inset-1.5"></span>
